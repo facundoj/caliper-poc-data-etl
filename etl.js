356eafa82d9a640d1664e1c9877d3a0325b51a17
facundoj <jaureguifacundo@gmail.com>
2016-01-27 17:19:57 -0300

    Updating

--- etl.js
+++ etl.js
@@ -2,42 +2,36 @@ var fs      = require('fs'),
     path    = require('path'),
     rimraf  = require('rimraf');
 
-// ID,NAME,VERSION,ATTEMPT_ID,ATTEMPT_COUNT,RESULT_ID,SCORE,TOTAL
-var ASSESSMENT_PATTERN = /^(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+)/;
-// ID,RESPONSE_ID,RESPONSE_TYPE,RESPONSE_VALUE,ASSESSMENT_ID,ATTEMPT_ID,LO_ID,RESULT_ID,SCORE,TOTAL
-var ASSESSMENT_ITEM_PATTERN = /^(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+),(.+)/;
-
+// identity_id,login_identity_id,school_id,assessment_id,assessment_version,
+// 1,          2,                3,        4,            5,
+// attempt_id,assmtitem_id,assmtitem_version,assessment_type_id,response_type,question_time,
+// 6,         7,           8,                9,                 10,           11,
+// score_posible,score_earned,masterobjectives,masterobjectivesid,objectivenumber
+// 12,           13,          14,              15,                16
+var ASSESSMENT_ITEM_PATTERN = /^([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),(.*),([0-9\.]*),([0-9\.]*)/;
 var assessments = null,
-    items = null,
+    itemsGroupedByAttempt = new Map(),
     targetReady = false;
 
-if (process.argv.length < 4) {
-    console.log('Usage: node etl.js <assessments-csv> <assessment-items-csv>');
+if (process.argv.length < 3) {
+    console.log('Usage: node etl.js <assessment-items-csv>');
     return;
 }
 
-// Reading Assessments
+// Reading Assessement Items
 fs.readFile(path.normalize(process.argv[2]), 'utf-8', function(err, data) {
     if (err) {
         console.log('Failed to read file.');
         return;
     }
 
-    assessments = data;
-    start();
-});
-
-// Reading Assessement Items
-fs.readFile(path.normalize(process.argv[3]), 'utf-8', function(err, data) {
-    if (err) {
-        console.log('Failed to read file.');
-        return;
-    }
-
-    items = data.trim()
+    console.log('[ITEMS] Starting process.. (' + new Date() + ')');
+    data.trim()
         .split('\n')
         .filter(function(line, i) { return i > 0; }) // remove column names
-        .map(formatAssessmentItemsElements);
+        .forEach(formatAssessmentItemsElements);
+
+    console.log('[ITEMS] Done! (' + new Date() + ')');
 
     start();
 });
@@ -52,154 +46,81 @@ rimraf('target', function(err) {
 
 // Start ETL
 function start() {
-    if (!(assessments && items && targetReady)) {
+    if (!(itemsGroupedByAttempt.size > 0 && targetReady)) {
+        console.log("no");
         return;
     }
 
-    var outputFilesData = assessments.trim()
-        .split('\n')
-        .filter(function(line, i) { return i > 0; }) // remove column names
-        .map(formatAssessmentElements) // [ [{events},..], [{events},..], ... ]
+    console.log('[ATTEMPTS] Starting attempts..(' + new Date() + ')');
 
-    // Writting files
-    outputFilesData.forEach(function(events, i) {
-        var filename = path.normalize('./target/events-' + i + '-' + Date.now() + '.json');
-        fs.writeFile(filename, JSON.stringify(events), function(err) {
-            if (err) {
-                console.log('Couldn\'t create ' + filename);
-            } else {
-                console.log('Created ' + filename);
-            }
-        });
+    var i = 0;
+    itemsGroupedByAttempt.forEach(function (value, attempt) {
+        i++;
+        formatAssessmentElementsAndPrint(value, i);
     });
+
+    console.log('[ATTEMPTS] Done! (' + new Date() + ')');
 }
 
 function formatAssessmentItemsElements(line) {
-    // ID,RESPONSE_ID,RESPONSE_TYPE,RESPONSE_VALUE,ASSESSMENT_ID,ATTEMPT_ID,LO_ID,RESULT_ID,SCORE,TOTAL
-    // 1, 2,          3,            4,             5,            6,         7,    8,        9,    10
+    // identity_id,login_identity_id,school_id,assessment_id,assessment_version,
+    // 1,          2,                3,        4,            5,
+    // attempt_id,assmtitem_id,assmtitem_version,assessment_type_id,response_type,question_time,
+    // 6,         7,           8,                9,                 10,           11,
+    // score_posible,score_earned,masterobjectives,masterobjectivesid,objectivenumber
+    // 12,           13,          14,              15,                16
     // Pulling values from assessment item row
     var res = line.trim().match(ASSESSMENT_ITEM_PATTERN);
     if (!res) return null;
 
-    // Details JSON structure for AssessmentItemEvent
-    var assessmentItemEvent = createEvent('ASSESSMENT_ITEM_EVENT', {
-        action: 'COMPLETED',
-        object: {
-            id: res[1]
-        },
-        generated: {
-            id: res[2],
-            type: res[3],
-            value: res[4]
-        },
-        isPartOf: {
-            // assessment id
-            id: res[5]
-        },
-        learningObjective: []
-    });
-
     // Supports n Learning Objects splitted by ";"
-    res[7].trim().split(';').forEach(function(id) {
-        assessmentItemEvent.values.learningObjective.push({
+    var learningObjectives = [];
+    res[15].trim().split(';').forEach(function(id) {
+        learningObjectives.push({
             id: id
         });
     });
 
     var outcomeEvent = createEvent('OUTCOME_EVENT', {
         action: 'GRADED',
-        target: {
+        actor: {
             id: res[1]
+        },
+        target: {
+            id: res[7],
+            learningObjectives
         },
         object: {
             id: res[6]
         },
         generated: {
-            id: res[8],
-            normalScore: res[9],
-            totalScore: res[10]
+            normalScore: res[13],
+            totalScore: res[12]
+        },
+        assessment: {
+            id: res[4],
+            version: res[5]
         }
     });
 
-    return {
-        assessmentId: res[5],
-        assessmentInfo: assessmentItemEvent,
-        assessmentOutcomeInfo: outcomeEvent
-    };
+    var attemptKey = res[6];
+    if (!itemsGroupedByAttempt.has(attemptKey)) {
+        itemsGroupedByAttempt.set(attemptKey, []);
+    }
+
+    itemsGroupedByAttempt.get(attemptKey).push(outcomeEvent);
 }
 
-function formatAssessmentElements(line) {
-    // ID,NAME,VERSION,ATTEMPT_ID,ATTEMPT_COUNT,RESULT_ID,SCORE,TOTAL
-    // 1, 2,   3,      4,         5,            6,        7,    8
-    // Pulling values from assessment row
-    var res = line.trim().match(ASSESSMENT_PATTERN);
-    if (!res) return null;
+function formatAssessmentElementsAndPrint(line, i) {
+    var filename = path.normalize('./target/events-' + i + '-' + Date.now() + '.json');
 
-    var eventsChain = [];
-
-    eventsChain.push(createEvent('ASSESSMENT_EVENT', {
-        action: 'STARTED',
-        object: {
-            id: res[1],
-            name: res[2],
-            version: res[3]
-        },
-        generated: {
-            id: res[4],
-            count: res[5]
+    fs.writeFile(filename, JSON.stringify(line), function(err) {
+        if (err) {
+            console.log('Couldn\'t create ' + filename);
+        } else {
+            console.log('Created ' + filename);
         }
-    }));
-
-    // Left joining by assessment id
-    var filteredItems = items.filter(function(itemEvent) {
-        return itemEvent.assessmentId == res[1];
     });
-
-    // Assessment Item Event
-    filteredItems.map(function(item) {
-            // Get assessment data only
-            return item.assessmentInfo;
-        }).forEach(function(assessmentItemEvent) { // Adding to the chain
-            eventsChain.push(assessmentItemEvent);
-        });
-
-    eventsChain.push(createEvent('ASSESSMENT_EVENT', {
-        action: 'SUBMITTED',
-        object: {
-            id: res[1],
-            name: res[2],
-            version: res[3]
-        },
-        generated: {
-            id: res[4],
-            count: res[5]
-        }
-    }));
-
-    eventsChain.push(createEvent('OUTCOME_EVENT', {
-        action: 'GRADED',
-        target: {
-            id: res[1]
-        },
-        object: {
-            id: res[4]
-        },
-        generated: {
-            id: res[6],
-            normalScore: res[7],
-            totalScore: res[8]
-        }
-    }));
-
-
-    filteredItems.map(function(item) {
-            // Get assessment data only
-            return item.assessmentOutcomeInfo;
-        }).forEach(function(assessmentItemEvent) { // Adding to the chain
-            eventsChain.push(assessmentItemEvent);
-        });
-
-    return eventsChain;
 }
 
 function createEvent(type, details) {
